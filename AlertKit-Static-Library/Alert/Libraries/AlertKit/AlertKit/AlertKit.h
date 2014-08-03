21b206dbf48b0130465b3dfa96279607e7f68e8d
Rob Ringham <rringham@infusion.com>
2014-08-03 11:15:40 -0400

    Initial version of AlertKit library

---
+++ AlertKit-Static-Library/Alert/Libraries/AlertKit/AlertKit/AlertKit.h
@@ -0,0 +1,13 @@
+//
+//  AlertKit.h
+//  AlertKit
+//
+//  Created by Rob Ringham on 8/3/14.
+//  Copyright (c) 2014 Rob Ringham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlertKit : NSObject
+
+@end
---
+++ AlertKit-Static-Library/Alert/Libraries/AlertKit/AlertKit/AlertKit.m
@@ -0,0 +1,13 @@
+//
+//  AlertKit.m
+//  AlertKit
+//
+//  Created by Rob Ringham on 8/3/14.
+//  Copyright (c) 2014 Rob Ringham. All rights reserved.
+//
+
+#import "AlertKit.h"
+
+@implementation AlertKit
+
+@end
